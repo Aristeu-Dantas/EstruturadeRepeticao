15fef877d37cb1121eee5dd9cec68ff6ff2a9a6f
Aristeu Dantas <82111816+Aristeu-Dantas@users.noreply.github.com>
2022-05-19 18:54:18 -0300

    Update q8.py

--- q8.py
+++ q8.py
@@ -6,9 +6,4 @@ n3=int(input('Informe o número 3: '))
 n4=int(input('Informe o número 4: '))
 n5=int(input('Informe o número 5: '))
 
-#lista=[n1,n2,n3,n4,n5]
-#x=(n1+n2+n3+n4+n5)/5
 print(f'A soma é {(n1+n2+n3+n4+n5)} e a média é {(n1+n2+n3+n4+n5)/5}')
-# total=0
-
-# print(sum(lista))

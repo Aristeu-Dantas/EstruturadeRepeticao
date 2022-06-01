fc7e318ce48c029acd3c32813cf4cf5f324225b7
Aristeu Dantas <82111816+Aristeu-Dantas@users.noreply.github.com>
2022-06-01 19:34:51 -0300

    Update q20.py

--- q20.py
+++ q20.py
@@ -1,17 +1,15 @@
 # Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o fatorial
 # várias vezes e limitando o fatorial a números inteiros positivos e menores que 16. 
 
-# numerof=int(input('Número de 1 à 16 para fatorar: '))
-
-resultado=1
-count=1
+#Definindo resposta de continuação
 rep='S'
-
+#Lógica para perguntar várias vezes
 while rep=='S':
-    # resultado=1
+    resultado=1
+    count=1
     numerof=int(input('Número de 1 à 16 para fatorar: '))
-    if numerof>0 and numerof<16:
-        while numerof>=count:
+    if numerof>0 and numerof<16:#Determinção de limite 1 à 16
+        while numerof>=count:#Lógica de fatoração
             resultado*=count
             count+=1
         print(resultado)
@@ -21,4 +19,3 @@ while rep=='S':
         break
 else:
     print('Programa encerrado.')
-#rep=input('Deseja continuar?(S/n) ')
